b2d78463bca0efdbbae314044861a812eb819937
fabiogif <fabiosantanagif@gmail.com>
2019-07-30 18:20:40 -0300

    novas app

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+import { Container } from './styles'
+
+export default function Header() {
+    return (
+        <Container>
+            <h1>Rocket Fy</h1>
+        </Container>
+    )
+}
